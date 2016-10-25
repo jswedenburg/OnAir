19f6dd0be66996831ee08736f54f4b0b99117b27
Angel Contreras <angel.contreras23@me.com>
2016-10-25 15:19:03 -0600

    setup the tableview delegate methods

--- OnAir/BroadCastViewController.swift
+++ OnAir/BroadCastViewController.swift
@@ -7,30 +7,62 @@
 //
 
 import UIKit
+import MultipeerConnectivity
 
-class BroadCastViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+class BroadCastViewController: UIViewController {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        MPCManager.sharedController.advertiser.startAdvertisingPeer()
+        MPCManager.sharedController.browser.startBrowsingForPeers()
     }
     
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+}
+
+
+// MARK: - TableView Delegate and Datasource
+extension ReceiverMusicPlayerViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return MPCManager.sharedController.foundPeers.count
     }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "broadcastCell", for: indexPath)
+        
+        let peer = MPCManager.sharedController.foundPeers[indexPath.row]
+        cell.textLabel?.text = peer.displayName
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // TODO: Present ReceiverMusicPlayer
+    }
+    
+}
 
-   
-
+extension ReceiverMusicPlayerViewController: MPCManagerDelegate{
+    func foundPeer(){
+        tableView.reloadData()
+    }
 }
