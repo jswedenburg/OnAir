b20914e0b175372a55fe6569705d512aa45ec6e4
jswedenburg <jswedenburg@gmail.com>
2016-10-25 11:13:54 -0600

    Subclassed VC, and TVCell.

--- OnAir/BroadCastViewController.swift
+++ OnAir/BroadCastViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class BroadCastViewController: UIViewController {
+class BroadCastViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,15 +21,16 @@ class BroadCastViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+
+   
 
 }
---
+++ OnAir/BroadcastTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BroadcastTableViewCell.swift
+//  OnAir
+//
+//  Created by Jake SWEDENBURG on 10/25/16.
+//  Copyright © 2016 Jake Swedenbug. All rights reserved.
+//
+
+import UIKit
+
+class BroadcastTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
