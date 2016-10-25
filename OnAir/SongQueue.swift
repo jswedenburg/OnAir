9b2624dc7881bebc84af0376a2c0fa90ba1b3a34
avanalfen <austinvanalfen@Van-Alfen-Mac.local>
2016-10-25 14:00:40 -0600

    Added Model Objects
    Song
    Album
    SongQueue

---
+++ OnAir/SongQueue.swift
@@ -0,0 +1,18 @@
+//
+//  SongQueue.swift
+//  OnAir
+//
+//  Created by Austin Van Alfen on 10/25/16.
+//  Copyright © 2016 Jake Swedenbug. All rights reserved.
+//
+
+import Foundation
+
+class SongQueue {
+    
+    var songs: [Song]
+    
+    init(songs: [Song] = []) {
+        self.songs = songs
+    }
+}
