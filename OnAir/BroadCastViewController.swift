b86a4f383d8ad49616cbba0ab9f8b658aee763ce
jswedenburg <jswedenburg@gmail.com>
2016-10-25 11:10:20 -0600

    Embeded in TabBar. Subclassed.

---
+++ OnAir/BroadCastViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BroadCastViewController.swift
+//  OnAir
+//
+//  Created by Jake SWEDENBURG on 10/25/16.
+//  Copyright © 2016 Jake Swedenbug. All rights reserved.
+//
+
+import UIKit
+
+class BroadCastViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
