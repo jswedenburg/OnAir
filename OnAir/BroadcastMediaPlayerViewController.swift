05857c21ee82b11692644942e1ca1530c29b44cd
jswedenburg <jswedenburg@gmail.com>
2016-10-26 12:05:30 -0600

    Added the VC

---
+++ OnAir/BroadcastMediaPlayerViewController.swift
@@ -0,0 +1,21 @@
+//
+//  BroadcastMediaPlayerViewController.swift
+//  OnAir
+//
+//  Created by Jake SWEDENBURG on 10/26/16.
+//  Copyright © 2016 Jake Swedenbug. All rights reserved.
+//
+
+import UIKit
+
+class BroadcastMediaPlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+            }
+    
+    
+    
+
+}
