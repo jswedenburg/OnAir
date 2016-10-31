f709c362eaba75a7bc7dfc138eb73658ce2239a4
Jake Swedenburg <jswdenburg@gmail.com>
2016-10-31 07:37:40 -0600

    Tableview with connected peers implemented

--- OnAir/BroadcastMusicPlayerViewController.swift
+++ OnAir/BroadcastMusicPlayerViewController.swift
@@ -9,17 +9,19 @@
 import UIKit
 import MultipeerConnectivity
 
-class BroadcastMusicPlayerViewController: UIViewController {
+class BroadcastMusicPlayerViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    //MARK: Properties
     var playMode = true
     
     
-
+    //MARK: View Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
-            }
+    }
     
+    
+    //MARK: Actions
     @IBAction func playButtonPressed(){
         if playMode{
             MusicPlayerController.sharedController.broadcasterPause()
@@ -30,7 +32,6 @@ class BroadcastMusicPlayerViewController: UIViewController {
             sendPlayData()
             playMode = true
         }
-        
     }
     
     @IBAction func nextButtonPressed() {
@@ -38,8 +39,25 @@ class BroadcastMusicPlayerViewController: UIViewController {
         sendNextSongData()
     }
     
+    //MARK TableView Datasource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return MPCManager.sharedController.connectedPeers.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "peerCell", for: indexPath)
+        let peer = MPCManager.sharedController.connectedPeers[indexPath.row]
+        cell.textLabel?.text = peer.displayName
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Listening Peers"
+    }
     
     
+    //MARK: Helper Functions
     func sendPlayData() {
         let messageDict: [String: String] = ["instruction": "play"]
         MPCManager.sharedController.sendData(dictionary: messageDict)
